c8e1a1274f4175cd639bd5c43fc51d65d44cec3b
Michael Sklepowich <michaelsklepowich@Michaels-MBP-3.localdomain>
2018-04-24 11:42:40 -0700

    adding initial server.js scaffold with node modules

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const cors = require('cors');
+const pg = require('pg');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT;
+const CLIENT_URL  = process.env.CLIENT_URL;
+
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
+
+app.use(cors());
+
+app.get('/', (req, res) => res.send('wat'));
+
+app.get('/api/v1/tasks', (req, res) => {
+  client.query(`SELECT * FROM tasks;`)
+  .then(results => res.send(results.rows))
+  .catch(console.error(err));
+});
+
+app.get('/test', (req,res) => res.send('this is the test'));
+app.get('*', (req,res) => res.redirect(CLIENT_URL));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
