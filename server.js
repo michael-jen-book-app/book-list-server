cfebdef764266b5cc9d15d58fc0c606d2bdc0e0c
Jennifer Bach <jmbach1@gmail.com>
2018-04-24 13:30:16 -0700

    added database and connections to heroku

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const cors = require('cors');
 const pg = require('pg');
 const express = require('express');
 const app = express();
+const conString = 'postgres://msudvrbmjkdmli:472775a4021a819c95b739534ba6bda9a620baab0f8b0e84c2a9ae9b4c8ed406@ec2-50-16-196-238.compute-1.amazonaws.com:5432/dcck59nhk494b5'
 
 const PORT = process.env.PORT;
-const CLIENT_URL  = process.env.CLIENT_URL;
+const CLIENT_URL  = process.env.
 
-const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client(process.env.conString);
 client.connect();
 client.on('error', err => console.error(err));
 
