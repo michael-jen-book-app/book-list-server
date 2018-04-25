5efd7c6f74e0ef6b41ed7b80394199f045fb0968
Michael Sklepowich <michaelsklepowich@Michaels-MBP-3.localdomain>
2018-04-25 13:49:37 -0700

    working server file & database

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const pg = require('pg');
 const express = require('express');
 const app = express();
 
-const PORT = proess.env.PORT;
+const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 
 const client = new pg.Client(process.env.DATABASE_URL);
@@ -14,16 +14,15 @@ client.on('error', err => console.error(err));
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send(''));
+app.get('/', (req, res) => res.redirect(CLIENT_URL));
 
-app.get('/api/v1/books_app', (req, res)) => {
+app.get('/api/v1/books', (req, res) => {
   client.query('SELECT * FROM books;')
   .then(results => res.send(results.rows))
-  .catch(console.error(err));
-}
+  .catch((err) => console.error(err));
+});
 
-app.get('/test', (req, res) => res.send('this is a tet'));
-app.get('*', (req, res) => res.send('404 page not found');
+app.get('/test', (req, res) => res.send('this is a test'));
+app.get('*', (req, res) => res.send('404 page not found'));
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
-
