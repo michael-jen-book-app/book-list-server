2d8448acae60be9c6818d5d02b017a937f99ff00
Jennifer Bach <jmbach1@gmail.com>
2018-04-25 10:12:01 -0700

    added connections to servers

--- server.js
+++ server.js
@@ -4,26 +4,26 @@ const cors = require('cors');
 const pg = require('pg');
 const express = require('express');
 const app = express();
-const conString = 'postgres://msudvrbmjkdmli:472775a4021a819c95b739534ba6bda9a620baab0f8b0e84c2a9ae9b4c8ed406@ec2-50-16-196-238.compute-1.amazonaws.com:5432/dcck59nhk494b5'
 
-const PORT = process.env.PORT;
-const CLIENT_URL  = process.env.
+const PORT = proess.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
 
-const client = new pg.Client(process.env.conString);
+const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('wat'));
+app.get('/', (req, res) => res.send(''));
 
-app.get('/api/v1/tasks', (req, res) => {
-  client.query(`SELECT * FROM tasks;`)
+app.get('/api/v1/books_app', (req, res)) => {
+  client.query('SELECT * FROM books;')
   .then(results => res.send(results.rows))
   .catch(console.error(err));
-});
+}
 
-app.get('/test', (req,res) => res.send('this is the test'));
-app.get('*', (req,res) => res.redirect(CLIENT_URL));
+app.get('/test', (req, res) => res.send('this is a tet'));
+app.get('*', (req, res) => res.send('404 page not found');
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
